9f109411fd0bb17ee354e3f33607b714fa4b46c6
Carlos Farias <carlos.farias@gft.com>
2019-07-27 11:27:49 +0200

    fixbug 'salir' function

--- app/controller/pegaso.controller.php
+++ app/controller/pegaso.controller.php
@@ -36,13 +36,10 @@ class pegaso_controller{
 		$data= new pegaso;
 		$salir=$data->salir();
 		session_unset();
-	        session_destroy();
-        	if (ini_get("session.use_cookies")) {
-            		$params = session_get_cookie_params();
-            		setcookie(session_name(), '', time() - 42000,
-                		$params["path"], $params["domain"],
-                		$params["secure"], $params["httponly"]
-            		);
+		session_destroy();
+		if (ini_get("session.use_cookies")) {
+			$params = session_get_cookie_params();
+//			setcookie(session_name(), '', time() - 42000, $params["path"], $params["domain"], $params["secure"], $params["httponly"]);
 		}		
 		return;
 	}
