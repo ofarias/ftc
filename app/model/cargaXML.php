722a9f62bfee6347a45ffddd1787c5694e22fa76
Adan <azavala@ftcenlinea.com>
2019-08-05 06:12:49 -0500

    Insercion de Metadatos, actualizacion de los UUID Cancelados, y procesar los metadatos

--- app/model/cargaXML.php
+++ app/model/cargaXML.php
@@ -91,8 +91,7 @@ class cargaXML extends database {
 		return array("status"=>'no',"mensaje"=>'No se encontro el Archivo', "archivo"=>'no');
 	}
 
-	function insertarMetaDatos($archivo){
-		echo $archivo;
+	function leeMetaDatos($archivo){
 		$fp=fopen($archivo,'r');
 		$l=0;
 		$r=0;
@@ -100,20 +99,59 @@ class cargaXML extends database {
 			$linea = fgets($fp);
 			if($l > 0){
 				$d=explode("~", utf8_encode($linea));
-				$this->query="SELECT * FROM FTC_META_DATOS WHERE UUID = '$d[0]'";
-				$rs=$this->EjecutaQuerySimple();
-				$row=ibase_fetch_object($rs);
-				if(empty($row)){
+				$rfce=$d[1];
+				$rfcr=$d[3];
+				$rfc=$_SESSION['rfc'];
+				if($rfce == $rfc or $rfcr == $rfc){
+					//echo 'Pertenece a la empresa actual <br/>';
+					return array("status"=>'ok', "lineas"=>$l,"mensaje"=>'Se encontro el rfc en la primer linea.',"rfce"=>$rfce, "rfcr"=>$rfcr);
+				}else{
+					//echo 'El Arvhivo NO  Pertenece a la empresa actual<br/>';
+					return array("status"=>'No', "lineas"=>$l,"mensaje"=>'Al parecer el archivo no es de la empresa seleccionada',"rfce"=>$rfce, "rfcr"=>$rfcr);
+				}
+			}
+			$l++;
+		}
+		fclose($fp);
+	}
+
+	function insertarMetaDatos($archivo){
+		//echo $archivo;
+		$usuario = $_SESSION['user']->NOMBRE;
+		$fp=fopen($archivo,'r');
+		$l=0;
+		$r=0;
+		$this->query="SELECT * FROM FTC_META_DATOS WHERE ARCHIVO='$archivo'";
+		$rs=$this->EjecutaQuerySimple();
+		$row=ibase_fetch_object($rs);
+		if(!empty($row)){
+			echo 'El Archivo <b>'.$archivo.'</b> ya fue Cargado por <b>'.$row->USUARIO.'</b> el <b>'. $row->FECHA_CARGA.'</b><br/>';
+			return;
+		}							
+		while(!feof($fp)) {
+			$linea = fgets($fp);
+			if($l > 0){
+				$d=explode("~", utf8_encode($linea));
+				//echo 'Valor de la linea '.count($d).'<br/>';
+				if(count($d)>2){
 					if(strlen($d[11]) > 2){
 						$fc = "'".trim($d[11])."'";
 					}else{
 						$fc = 'null';
 					}
-					$this->query="INSERT INTO FTC_META_DATOS (IDMD, UUID, RFCE, NOMBRE_EMISOR, RFCR, NOMBRE_RECEPTOR, RFCPAC, FECHA_EMISION, FECHA_CERTIFICACION, MONTO, EFECTO_COMPROBANTE, STATUS, FECHA_CANCELACION) 
-								VALUES (NULL, '$d[0]', '$d[1]', '$d[2]', '$d[3]', '$d[4]', '$d[5]', '$d[6]','$d[7]', $d[8], '$d[9]', $d[10], ".$fc.")";
+					$this->query="INSERT INTO FTC_META_DATOS (IDMD, UUID, RFCE, NOMBRE_EMISOR, RFCR, NOMBRE_RECEPTOR, RFCPAC, FECHA_EMISION, FECHA_CERTIFICACION, MONTO, EFECTO_COMPROBANTE, STATUS, FECHA_CANCELACION, ARCHIVO, FECHA_CARGA, USUARIO, PROCESADO) 
+									VALUES (NULL, '$d[0]', '$d[1]', '$d[2]', '$d[3]', '$d[4]', '$d[5]', '$d[6]','$d[7]', $d[8], '$d[9]', $d[10], ".$fc.", '$archivo', current_timestamp, '$usuario', 0)";
 					$res=$this->grabaBD();
-					if($res == 1){
-						$r+=$res;	
+					if($res==1){
+						$r+=$res;
+						if(strlen($d[11]) > 2){
+							$this->query="UPDATE XML_DATA SET STATUS = 'C' WHERE UPPER(UUID) = UPPER('$d[0]')";
+							$r=$this->queryActualiza();
+							if($r==1){
+								$this->query="UPDATE FTC_META_DATOS SET PROCESADO = 1 WHERE UPPER(UUID) = UPPER('$d[0]') AND FECHA_CANCELACION IS NOT NULL";
+								$this->queryActualiza();
+							}
+						}
 					}else{
 						echo '<br/>'.$this->query.'<br/>';
 					}
