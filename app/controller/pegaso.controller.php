d1cfab2d31cf48bfdbc1585455668fc593e1d5a8
Oscar Farias Ayala <ofarias@ftcenlinea.com>
2020-02-25 13:17:56 -0600

    Ajuste para que jale la cuenta correcta en los Pagos del estado de cuenta.

--- app/controller/pegaso.controller.php
+++ app/controller/pegaso.controller.php
@@ -13409,6 +13409,9 @@ function ImpSolicitud2($idsol){
 	            $fin=$data->entidades();
 	            $total=$data->montoAplicado($idp);
 	            $pago=$data->infoPago($idp);
+	            foreach ($pago as $i) {
+	            	$a=date("y", strtotime($i->FECHA_RECEP));
+	            }
 	            $facturas=$data->pagoFacturas($idp);
 	            foreach ($pago as $x) {
 	            	$x=$x->MONTO;
