806ff5df81d79cf3a0a149768c8c6f36c8241ec9
Oscar Farias <ofarias@ftcenlinea.com>
2023-05-16 21:28:32 -0600

    Pruebas Debian 11 - 14

--- app/model/database.php
+++ app/model/database.php
@@ -12,6 +12,7 @@
 			$host = 'ofa.dyndns.org:'.$_SESSION['bd'];
 			//$path = "C:\\users\\gense\\desktop\\Datos\\";
 			//$host = $path.$_SESSION['folder'].'\\'.$_SESSION['bd'];
+			phpinfo();
 			$this->cnx=ibase_connect($host, self::$usr, self::$pwd);
 		}
 		#Cierra la conexion a la base de datos
